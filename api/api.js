a4290090d89579e9bfc0dc1bc4258173b9698529
lazaro portugal <portugallazaro@gmail.com>
2020-02-01 14:46:53 -0300

    criacao do metodo que salva as informcoes do dev no banco de dados

--- api/api.js
+++ api/api.js
@@ -12,13 +12,28 @@ class Api extends Actions {
         if(await response.status == 200){
             var data = await response.json();
  
-             this.ul.classList.remove('disabled')
-             this.renderDev(data)
+            this.ul.classList.remove('disabled')
+            this.renderDev(data)
          } 
  
-         this.faz(data)
-         this.edit();
-     }
+        this.faz(data)
+        this.edit();
+    }
+
+
+    async save(bodyInfo) {
+       let response = await fetch(`${this.url}/devs`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(bodyInfo)
+        })
+
+        let data = await response.json();
+        return data
+    }
 
 
 }
