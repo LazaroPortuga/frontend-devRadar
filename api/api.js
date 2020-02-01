1c540929ae0da58e56f6e1643ee954e5ac81104b
lazaro portugal <portugallazaro@gmail.com>
2020-02-01 14:44:04 -0300

    criacao da classe que vai fazer as requisicoes no backend

---
+++ api/api.js
@@ -0,0 +1,12 @@
+class Api extends Actions {
+    constructor() {
+        super();
+        this.url = "http://localhost:3333";
+        this.getGata()
+        this.sendForm()
+    }
+
+}
+
+
+new Api()
